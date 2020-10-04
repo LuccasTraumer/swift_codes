c900077ee9f50924b5d2f1fbc3051cadff56c240
luccastraumer <cc19372@g.unicamp.br>
2020-10-04 16:28:40 -0400

    initial commit

---
+++ screens/screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  screens
+//
+//  Created by user182163 on 10/4/20.
+//  Copyright © 2020 user182163. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
